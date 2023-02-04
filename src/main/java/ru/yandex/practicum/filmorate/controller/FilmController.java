484d0bcf265f1445baab14f67cf85be5faaf3c5c
winmord <winmord@yandex.ru>
2023-02-04 14:46:28 +0300

    feature: добавить эндпоинты FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,35 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.FilmAlreadyExistsException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
 
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        if (films.containsKey(film.getId())) {
+            throw new FilmAlreadyExistsException("Фильм с id " + film.getId() + " уже существует");
+        }
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.Instant;
 
 @Data
 public class Film {
     Integer id;
+
+    @NonNull
     String name;
+
     String description;
     Instant releaseDate;
     Integer duration;
