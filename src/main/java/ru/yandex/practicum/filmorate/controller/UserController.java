7bf8a79a104d5b1f87e9a85119e48b775c10d3a2
winmord <winmord@yandex.ru>
2023-02-04 14:50:29 +0300

    feature: добавить эндпоинты UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,36 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.UserAlreadyExistsException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getAllUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        if (users.containsKey(user.getId())) {
+            throw new UserAlreadyExistsException("Пользователь с id " + user.getId() + " уже существует");
+        }
+
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
 }
